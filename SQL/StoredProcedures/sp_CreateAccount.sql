905591222b57f0f6085451962fbeacefa8fa918b
Allan2Fernandes <86521972+Allan2Fernandes@users.noreply.github.com>
2023-09-13 10:30:34 +0200

    Fixed a misalignment between the SPs in the project and on the sql server

--- SQL/StoredProcedures/sp_CreateAccount.sql
+++ SQL/StoredProcedures/sp_CreateAccount.sql
@@ -16,27 +16,27 @@ description:        the stored procedure creates an with the specified username 
 */
 
 
-alter procedure [dbo].[sp_createaccount]
-	@username nvarchar(200),
-	@password nvarchar(200),
-	@accesslevel nvarchar(10),
-	@overrideusernamecheck bit = 0
-		as
-			begin
-				-- search for existing usernames in the database
-				if(exists(select * from [go_pvg32block].[dbo].[user_string] where paramid = 39901 and paramvalue = @username) and @overrideusernamecheck = 0)
-					begin
-						select '0' as 'created user'
-					end
-				else
-					begin
-						select '1' as 'created user'
-						declare @dynamicstring nvarchar(100);
-						set @dynamicstring = concat('39901;', @username, ';15002;', @accesslevel, ';string39902;', @password);
-						execute sp_saveparams 1, 'user', @dynamicstring;
-					end
+CREATE PROCEDURE [dbo].[sp_CreateAccount]
+	@Username NVARCHAR(200),
+	@Password NVARCHAR(200),
+	@AccessLevel NVARCHAR(10),
+	@OverrideUserNameCheck BIT = 0
+		AS
+			BEGIN
+				-- Search for existing usernames in the database
+				IF(EXISTS(SELECT * FROM [GO_PVG32BLOCK].[dbo].[User_STRING] WHERE ParamID = 39901 AND ParamValue = @Username) AND @OverrideUserNameCheck = 0)
+					BEGIN
+						SELECT '0' AS 'Created User'
+					END
+				ELSE
+					BEGIN
+						SELECT '1' AS 'Created User'
+						DECLARE @DynamicString NVARCHAR(100);
+						SET @DynamicString = CONCAT('39901;', @Username, ';15002;', @AccessLevel, ';STRING39902;', @Password);
+						EXECUTE sp_SaveParams 1, 'User', @DynamicString;
+					END
 
-			end
+			END
 go
 
 
