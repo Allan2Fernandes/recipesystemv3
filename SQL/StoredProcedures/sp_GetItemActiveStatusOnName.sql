d6126e6d58467d6052dac5083df0338fe2ab16e0
Allan2Fernandes <86521972+Allan2Fernandes@users.noreply.github.com>
2023-09-20 11:12:13 +0200

    Added 2 stored procedures which should have been added to the project from sql

---
+++ SQL/StoredProcedures/sp_GetItemActiveStatusOnName.sql
@@ -0,0 +1,67 @@
+USE [GO_PVG32BLOCK]
+GO
+
+/****** Object:  StoredProcedure [dbo].[sp_GetItemActiveStatusOnName]    Script Date: 20/09/2023 11.05.50 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+/****** Script for SelectTopNRows command from SSMS  ******/
+
+CREATE PROCEDURE [dbo].[sp_GetItemActiveStatusOnName]
+	@action nvarchar(50),
+	@itemName nvarchar(50)
+		AS
+			BEGIN
+				DECLARE @itemParamValue nvarchar(10)
+				IF @action ='Pick'
+						BEGIN
+							SET @itemParamValue = '4'
+						END
+					IF @action ='Expander'
+						BEGIN
+							SET @itemParamValue = '5'
+						END
+					IF @action ='Kolver'
+						BEGIN
+							SET @itemParamValue = '6'
+						END
+					IF @action ='Atlas'
+						BEGIN
+							SET @itemParamValue = '7'
+						END
+					IF @action ='Press'
+						BEGIN
+							SET @itemParamValue = '8'
+						END
+					IF @action ='Orientation'
+						BEGIN
+							SET @itemParamValue = '9'
+						END
+					IF @action ='Acknowledge'
+						BEGIN
+							SET @itemParamValue = '10'
+						END
+
+
+					SELECT ParamValue FROM Recipe_BOOL WHERE SetID =
+					(
+					SELECT MAX(SetID) FROM Recipe_STRING WHERE SetID IN
+					(SELECT SetID FROM Recipe_STRING WHERE ParamID = 30004 AND ParamValue = @itemParamValue)
+					AND ParamValue = @itemName
+					)
+
+			END
+
+
+
+
+GO
+
+
---
+++ SQL/StoredProcedures/sp_GetStepImage.sql
@@ -0,0 +1,75 @@
+USE [GO_PVG32BLOCK]
+GO
+
+/****** Object:  StoredProcedure [dbo].[sp_GetStepImage]    Script Date: 20/09/2023 11.09.43 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+
+
+
+CREATE PROCEDURE [dbo].[sp_GetStepImage]
+	@selectedRecipeSetID INT
+	,@stepNo INT
+		AS
+			BEGIN
+				SET NOCOUNT ON
+				-- SetIDs of the steps and sub steps. The sub steps could connect to recipes
+				DECLARE @list1 TABLE (
+					SetID INT
+				);
+				INSERT INTO @list1 (SetID)
+				SELECT SetID
+				FROM Recipe_DINT
+				WHERE ParamValue = @selectedRecipeSetID and ParamID = 15001;
+
+				-- Filter the SetIDs to only get StepIds
+				DECLARE @list2 TABLE (
+					SetID INT
+				);
+
+				INSERT INTO @list2
+				SELECT ri.SetID
+				FROM Recipe_INT ri
+				WHERE ri.SetID IN (SELECT l.SetID FROM @list1 l)
+					  AND ri.ParamID = 10002
+					  AND ri.ParamValue = 2;
+
+				-- Get the steps in the correct order
+				DECLARE @list3 TABLE (
+					SetID INT,
+					ParamID INT,
+					ParamValue INT
+				);
+
+				INSERT INTO @list3
+				SELECT *
+				FROM Recipe_INT
+				WHERE SetID IN (SELECT SetID FROM @list2)
+					  AND ParamID = 10003
+				ORDER BY ParamValue;
+
+
+				-- Recipes and their instructions
+				select
+					T1.ParamID,
+					--CASE WHEN T1.ParamID = 35001 OR T1.ParamID = 35002 THEN 'REMOVED IMAGE' ELSE T1.ParamValue END AS 'ParamValue',
+					T1.ParamValue
+				from Recipe_STRING T1 inner join @list3 T2
+				on T1.SetID = T2.SetID
+				where T1.SetID in (select SetID from @list3)
+				and T2.ParamValue=@stepNo
+				order by T2.ParamValue ASC
+
+			END
+GO
+
+
